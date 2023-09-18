ba8eb796422fa12b75ce22e5367996afef5091ef
nouhben <nouhben@MacBook-Pro.local>
2023-09-18 08:47:01 +0100

    adding models

---
+++ lib/models/rive_model.dart
@@ -0,0 +1,15 @@
+import 'package:rive/rive.dart';
+
+class RiveModel {
+  final String src, artboard, stateMachineName;
+  late SMIBool? status;
+
+  RiveModel({
+    required this.src,
+    required this.artboard,
+    required this.stateMachineName,
+    this.status,
+  });
+
+  set setStatus(SMIBool status) => this.status = status;
+}
