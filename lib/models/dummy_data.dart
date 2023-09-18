db9b30c1d80220e0136f71f4e35490424a81175e
nouhben <nouhben@MacBook-Pro.local>
2023-09-18 08:53:03 +0100

    adding models: menus & dummy data

---
+++ lib/models/dummy_data.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart' show Color;
+
+import 'course.dart';
+
+final List<Course> recentCourses = [
+  Course(title: "State Machine"),
+  Course(
+    title: "Animated Menu",
+    color: const Color(0xFF9CC5FF),
+    iconSrc: "assets/icons/code.svg",
+  ),
+  Course(title: "Flutter with Rive"),
+  Course(
+    title: "Animated Menu",
+    color: const Color(0xFF9CC5FF),
+    iconSrc: "assets/icons/code.svg",
+  ),
+  Course(
+    title: "Animations in SwiftUI",
+  ),
+  Course(
+    title: "Animations in Flutter",
+    iconSrc: "assets/icons/code.svg",
+    color: const Color(0xFF80A4FF),
+  ),
+];
---
+++ lib/models/menu.dart
@@ -0,0 +1,11 @@
+import 'package:animately/models/rive_model.dart';
+
+class Menu {
+  final String title;
+  final RiveModel rive;
+
+  Menu({
+    required this.title,
+    required this.rive,
+  });
+}
