7b3d972c45583e09f688321bde5b38c5b6b4dbb2
nouhben <nouhben@MacBook-Pro.local>
2023-09-19 17:54:10 +0100

    added home screen

--- lib/pages/entry_point/entry_point_screen.dart
+++ lib/pages/entry_point/entry_point_screen.dart
@@ -1,5 +1,12 @@
+import 'dart:math' show pi;
+
 import 'package:animately/models/menu.dart';
 import 'package:animately/models/menus.dart';
+import 'package:animately/pages/entry_point/widgets/bottom_nav_item.dart';
+import 'package:animately/pages/entry_point/widgets/menu_button.dart';
+import 'package:animately/pages/entry_point/widgets/side_bar.dart';
+import 'package:animately/utils/constants.dart';
+import 'package:animately/utils/rive_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
@@ -57,6 +64,124 @@ class _EntryPointState extends State<EntryPoint>
     _animationController.dispose();
     super.dispose();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      resizeToAvoidBottomInset: false,
+      backgroundColor: backgroundColor2,
+      body: Stack(
+        children: [
+          AnimatedPositioned(
+            width: 288,
+            height: MediaQuery.of(context).size.height,
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.fastOutSlowIn,
+            left: isSideBarMenuOpen ? 0 : -288,
+            top: 0,
+            child: const SideBar(),
+          ),
+          Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.001)
+              ..rotateY(
+                  1 * _animation.value - 30 * (_animation.value) * pi / 180),
+            child: Transform.translate(
+              offset: Offset(_animation.value * 265, 0),
+              child: Transform.scale(
+                scale: _scaleAnimation.value,
+                child: const ClipRRect(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(24),
+                  ),
+                  child: HomePage(),
+                ),
+              ),
+            ),
+          ),
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.fastOutSlowIn,
+            left: isSideBarMenuOpen ? 220 : 0,
+            top: 16,
+            child: MenuButton(
+              onTap: () {
+                isMenuOpenInput.value = !isMenuOpenInput.value;
+
+                if (_animationController.value == 0) {
+                  _animationController.forward();
+                } else {
+                  _animationController.reverse();
+                }
+
+                setState(() => isSideBarMenuOpen = !isSideBarMenuOpen);
+              },
+              riveOnInit: (artboard) {
+                final controller = StateMachineController.fromArtboard(
+                    artboard, "State Machine");
+
+                artboard.addController(controller!);
+
+                isMenuOpenInput =
+                    controller.findInput<bool>("isOpen") as SMIBool;
+                isMenuOpenInput.value = true;
+              },
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Transform.translate(
+        offset: Offset(0, 100 * _animation.value),
+        child: SafeArea(
+          child: Container(
+            padding:
+                const EdgeInsets.only(left: 12, top: 12, right: 12, bottom: 12),
+            margin: const EdgeInsets.symmetric(horizontal: 24),
+            decoration: BoxDecoration(
+              color: backgroundColor2.withOpacity(0.8),
+              borderRadius: const BorderRadius.all(Radius.circular(24)),
+              boxShadow: [
+                BoxShadow(
+                  color: backgroundColor2.withOpacity(0.3),
+                  offset: const Offset(0, 20),
+                  blurRadius: 20,
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ...List.generate(
+                  bottomNavItems.length,
+                  (index) {
+                    Menu navBar = bottomNavItems[index];
+                    return BottomNavigationItem(
+                      navBar: navBar,
+                      onTap: () {
+                        RiveUtils.chnageSMIBoolState(navBar.rive.status!);
+                        updateSelectedBtmNav(navBar);
+                      },
+                      riveOnInit: (artboard) {
+                        navBar.rive.status = RiveUtils.getRiveInput(artboard,
+                            stateMachineName: navBar.rive.stateMachineName);
+                      },
+                      selectedNav: selectedBottonNav,
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
